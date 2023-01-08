68170debf49b6b3f2c942aea4c7404a0a2810a2b
LochinbekWeb <orinboyevlochinbek595@gmail.com>
2023-01-08 18:08:11 +0500

    O'zgarishlar

---
+++ script.js
@@ -0,0 +1,41 @@
+const rasm = document.querySelector('#rasm')
+
+function JavaScript() {
+    rasm.src = './images/javascript.png'
+}
+
+function Java() {
+    rasm.src = './images/Java.png'
+}
+
+function HTML() {
+    rasm.src = './images/html.png'
+}
+
+function Css() {
+    rasm.src = './images/css.png'
+}
+
+function Php() {
+    rasm.src = './images/php.png'
+}
+
+function Bootstrap() {
+    rasm.src = './images/bootstrap.png'
+}
+
+function Python() {
+    rasm.src = './images/python.png'
+}
+
+function Cplus() {
+    rasm.src = './images/C++.png'
+}
+
+function Haskell() {
+    rasm.src = './images/haskell.png'
+}
+
+function Ruby() {
+    rasm.src = './images/ruby.jpg'
+}
